30bd434cb9907ffafd4c47e25d7644945e8e1095
subhojyoti22 <smukherjee27@wisc.edu>
2024-05-23 20:04:15 -0500

    Code latest update

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # code
 Interpretable Code
 
+#### Create Environment ####
+
+conda create -n code python=3.8
+conda activate code
+conda install matplotlib
+pip install ipdb
+conda install scikit-learn
+conda install pandas
+
+
+
+
+The main files are:
+
+1. code2.py
+2. code2_changing.py
+3. code2_heart.py
+4. code2_movielens.py
+5. code2_white_wine.py
+
+
 
 Please cite the following paper.
 
---
+++ code_archive/aistats2024/plot_graph.py
@@ -0,0 +1,130 @@
+# Imports and defaults
+import joblib
+from joblib import Parallel, delayed
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import linprog
+import time
+
+import ipdb
+import pickle
+
+mpl.style.use("classic")
+mpl.rcParams["figure.figsize"] = [6, 4]
+
+mpl.rcParams["axes.linewidth"] = 0.75
+mpl.rcParams["errorbar.capsize"] = 3
+mpl.rcParams["figure.facecolor"] = "w"
+mpl.rcParams["grid.linewidth"] = 0.75
+mpl.rcParams["lines.linewidth"] = 0.75
+mpl.rcParams["patch.linewidth"] = 0.75
+mpl.rcParams["xtick.major.size"] = 3
+mpl.rcParams["ytick.major.size"] = 3
+
+mpl.rcParams["pdf.fonttype"] = 42
+mpl.rcParams["ps.fonttype"] = 42
+mpl.rcParams["font.size"] = 10
+mpl.rcParams["axes.titlesize"] = "medium"
+mpl.rcParams["legend.fontsize"] = "medium"
+
+n = 5000  # horizon
+
+step = np.arange(1, n + 1)
+sube = (step.size // 10) * np.arange(1, 11) - 1
+
+mpl.rcParams["font.size"] = 7
+plt.figure(figsize=(6.8, 2.25))
+
+def linestyle2dashes(style):
+  if style == "--":
+    return (3, 3)
+  elif style == ":":
+    return (0.5, 2.5)
+  else:
+    return (None, None)
+
+algs = [
+  ("LinUCB", {}, "green", "-", "LinUCB"),
+  ("LinTS", {}, "yellowgreen", "-", "LinTS"),
+  ("LinGreedy", {}, "blue", "-", r"$\varepsilon$-greedy"),
+  ("LinExploreCommit", {}, "cyan", "-", "EtC"),
+  ("LinPhasedElim", {}, "orange", "-", "Elimination"),
+  ("CODE", {}, "red", "-", "CODE")]
+
+# # load data
+# with open('data_linear.pickle', 'rb') as f:
+# with open('data_movielens.pickle', 'rb') as f:
+with open('data_heart3.pickle', 'rb') as f:
+# with open('data_wine.pickle', 'rb') as f:
+    # Pickle the 'data' dictionary using the highest protocol available.
+    data = pickle.load(f)
+
+regret_algs, ce_algs = data[0], data[1]
+
+# print(regret_algs[0])
+
+# # simulation
+
+for alg in algs:
+  
+  # all runs for a single algorithm
+  # alg_class = globals()[alg[0]]
+  # regret, ce = evaluate(alg_class, alg[1], envs, n)
+  regret, ce = regret_algs[:, :, algs.index(alg)], ce_algs[:, :, algs.index(alg)]
+
+  # regret plot
+  plt.subplot(1, 2, 1)
+  cum_regret = regret.cumsum(axis=0)
+  plt.plot(step, cum_regret.mean(axis=1),
+    alg[2], dashes=linestyle2dashes(alg[3]), label=alg[4])
+  plt.errorbar(step[sube], cum_regret[sube, :].mean(axis=1),
+    cum_regret[sube, :].std(axis=1) / np.sqrt(cum_regret.shape[1]),
+    fmt="none", ecolor=alg[2])
+
+  # cross-entropy plot
+  plt.subplot(1, 2, 2)
+  cum_ce = ce.cumsum(axis=0)
+  plt.plot(step, cum_ce.mean(axis=1),
+    alg[2], dashes=linestyle2dashes(alg[3]))
+  plt.errorbar(step[sube], cum_ce[sube, :].mean(axis=1),
+    cum_ce[sube, :].std(axis=1) / np.sqrt(cum_ce.shape[1]),
+    fmt="none", ecolor=alg[2])
+
+plt.subplot(1, 2, 1)
+# plt.title("(a) Linear Bandits",  fontsize=11)
+# plt.title("(a) Movielens",  fontsize=11)
+plt.title("(a) Heart Failure", fontsize=11)
+# plt.title("(a) Wine Quality", fontsize=11)
+plt.xlabel("Round n", fontsize=9)
+plt.ylabel("Regret", fontsize=9)
+
+plt.legend(loc="lower right", ncol=2, frameon=False, fontsize=9)
+plt.yscale('log')
+# plt.ylim(1, 200000) # required in wine
+
+plt.subplot(1, 2, 2)
+# plt.title("(b) Linear Bandits",  fontsize=11)
+# plt.title("(b) Movielens",  fontsize=11)
+plt.title("(b) Heart Failure",  fontsize=11)
+# plt.title("(b) Wine Quality", fontsize=11)
+plt.xlabel("Round n", fontsize=9)
+plt.ylabel("Uncertainty loss", fontsize=9)
+plt.ylim(0.1, 100)
+plt.yscale('log')
+
+plt.tight_layout()
+
+# plt.savefig("Figures/Linear2.pdf", format="pdf", dpi=1200, bbox_inches=0)
+# plt.savefig("Figures/Linear2.png", format="png", dpi=1200, bbox_inches=0)
+
+# plt.savefig("Figures/Linear2_movielens.pdf", format="pdf", dpi=1200, bbox_inches=0)
+# plt.savefig("Figures/Linear2_movielens.png", format="png", dpi=1200, bbox_inches=0)
+
+plt.savefig("Figures/Linear2_heart.pdf", format="pdf", dpi=1200, bbox_inches=0)
+plt.savefig("Figures/Linear2_heart.png", format="png", dpi=1200, bbox_inches=0)
+
+# plt.savefig("Figures/Linear2_wine.pdf", format="pdf", dpi=1200, bbox_inches=0)
+# plt.savefig("Figures/Linear2_wine.png", format="png", dpi=1200, bbox_inches=0)
+
+plt.show()
